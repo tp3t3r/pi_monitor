32ddf25f1c9c7fc5c93af31b39523518ca2a2de0
Peter <peter@hevimetal>
2026-01-23 18:52:54 +0100

    Use MaxNLocator to avoid tick overflow

--- pi_monitor_web.py
+++ pi_monitor_web.py
@@ -350,10 +350,10 @@ def generate_graph(metric, hours=None, mobile=False):
         from matplotlib.ticker import MaxNLocator
         
         if hours:
-            ax.xaxis.set_major_locator(MinuteLocator(interval=10))
+            ax.xaxis.set_major_locator(MaxNLocator(nbins=6))
             ax.xaxis.set_major_formatter(DateFormatter('%H:%M'))
         else:
-            ax.xaxis.set_major_locator(HourLocator(interval=6))
+            ax.xaxis.set_major_locator(MaxNLocator(nbins=10))
             ax.xaxis.set_major_formatter(DateFormatter('%m-%d\n%H:%M'))
         
         if timestamps:
